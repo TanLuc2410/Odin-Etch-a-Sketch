4fc37344bb78073f8cd31d30d7e94937d5005c08
Tan Luck <149952439+TanLuc2410@users.noreply.github.com>
2024-04-17 13:50:24 +0700

    Rework The Divs-Making Function
    Use one parameter instead of two. Calculate the divs width and height in function.

--- javascript.js
+++ javascript.js
@@ -2,17 +2,23 @@ const container = document.querySelector(".container");
 
 const buttonToMakeGrid = document.createElement("button");
 buttonToMakeGrid.classList.add("gridButton");
-buttonToMakeGrid.innerText = "Click me!";
+buttonToMakeGrid.innerText = "Click me for a new grid!";
 document.body.appendChild(buttonToMakeGrid);
 
 
-function makeGrid(gridRows, gridCols) {
-    for(let i = 1; i <= (gridRows * gridCols); i++) {
-        let cell = document.createElement("div");
-        cell.innerText = i;
-        cell.classList.add("gridItem");
-        container.appendChild(cell);
 
+
+function makeGrid(gridSize) {
+    const cellSize = Math.min(960 / gridSize, 10);
+    container.textContent = "";
+    
+    for(let i = 1; i <= (gridSize * gridSize); i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("gridItem");
+        cell.style.width = `${cellSize}`;
+        cell.style.height = `${cellSize}`;
+
+        container.appendChild(cell);
         cell.addEventListener("mouseover", function() {
             this.style.backgroundColor = "black";
         });
@@ -21,5 +27,5 @@ function makeGrid(gridRows, gridCols) {
         });
     }
 }
-makeGrid(16, 16);
+makeGrid(16);
 
